af02540f40b7a8bacb3d84a7767059a626a8fc9e
venkateshrajaram06 <sunainatalluri@gmail.com>
2018-06-12 11:46:40 +0530

    Done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-document.body.innerHTML = "Hello world!";
+for(var i=0;i<100;i++) {
+    var row = document.createElement('div');
+    row.className = 'row';
+    for(var j=0;j<100;j++){
+        const col = document.createElement('div');
+        col.className = "column";
+        col.addEventListener('click',() =>{
+            col.style.background = 'red';
+        });
+        row.appendChild(col);
+
+    }
+    document.body.appendChild(row);
+}
