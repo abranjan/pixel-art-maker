c0b273d2d6cd619dd166f5dd95c08378147a50a7
venkateshrajaram06 <sunainatalluri@gmail.com>
2018-06-12 11:51:18 +0530

    Completed

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-for(var i=0;i<100;i++) {
+for(var i=0;i<30;i++) {
     var row = document.createElement('div');
     row.className = 'row';
-    for(var j=0;j<100;j++){
+    for(var j=0;j<30;j++){
         const col = document.createElement('div');
         col.className = "column";
         col.addEventListener('click',() =>{
-            col.style.background = 'red';
+            col.style.background = colorSelected;
         });
         row.appendChild(col);
 
     }
     document.body.appendChild(row);
 }
+
+let colorSelected;
+
+let color = ['red','blue','green','pink','violet','yellow','black','brown'];
+for(var i=0;i<1;i++) {
+    var row = document.createElement('div');
+    row.className = 'row';
+    for(let j=0;j<8;j++){
+        const col = document.createElement('div');
+        col.className = "colorSet";
+        col.style.background = color[j];
+        col.addEventListener('click',() =>{
+            colorSelected = color[j];
+            alert("This color is selected " +color[j]);
+        });
+        row.appendChild(col);
+    }
+    document.body.appendChild(row);
+}
+
+
